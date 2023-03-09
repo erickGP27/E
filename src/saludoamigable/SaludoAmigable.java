81295c23ffe627765b36714a495703cbbb3ca106
erickGP27 <125601903+erickGP27@users.noreply.github.com>
2023-03-09 14:35:59 -0600

    Add files via upload

---
+++ src/saludoamigable/SaludoAmigable.java
@@ -0,0 +1,25 @@
+package saludoamigable;
+import java.util.Scanner;
+
+/**
+ *
+ * @author maryse
+ *
+ * Scanner
+ * Este programa despliega un saludo Hola personalizado.
+ *
+ */
+
+public class SaludoAmigable {
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+        String nombre;
+        System.out.print("Introduce tu nombre: ");
+        nombre = stdIn.nextLine();
+        System.out.println("¡Hola " + nombre + "!");
+
+    }
+
+}
